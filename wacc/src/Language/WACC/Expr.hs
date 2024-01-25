b3eabd534d0a404caff6be9f6197f392660c209e
Daniel Trowbridge <daniel.trowbridge22@imperial.ac.uk>
2024-01-25 23:36:08 +0000

    refactor(ast): move literal representation into new AtomExpr type class

--- wacc/src/Language/WACC/Expr.hs
+++ wacc/src/Language/WACC/Expr.hs
@@ -1,15 +1,79 @@
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE GADTs #-}
 {-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE TypeFamilies #-}
 
 {- |
 WACC expressions.
 -}
-module Language.WACC.Expr (UnExpr (..), BinExpr (..)) where
+module Language.WACC.Expr
+  ( AtomExpr (..)
+  , UnExpr (..)
+  , BinExpr (..)
+  , ArrayElem (..)
+  )
+where
 
 import Data.Kind (Type)
 import Language.WACC.Annotation (Ann)
-import Language.WACC.WType (Ordered, WType (..))
+import Language.WACC.WType (Erasure, Ordered, WType (..))
+
+{- |
+WACC array elements.
+-}
+data
+  ArrayElem
+    (expr :: WType erasure -> Type)
+    (ident :: WType erasure -> Type)
+    (t :: WType erasure)
+  where
+  -- |
+  -- The array identifier and the first index.
+  --
+  -- > <ident>[<expr>]
+  Index1 :: ident (WArray t) -> expr WInt -> ArrayElem expr ident t
+  -- |
+  -- A subsequent index after the first.
+  --
+  -- > <ident>[<expr>][<expr>]...
+  -- >                ^^^^^^^^^^^
+  IndexN
+    :: ArrayElem expr ident (WArray t) -> expr WInt -> ArrayElem expr ident t
+
+{- |
+Atomic WACC expressions.
+-}
+class AtomExpr (expr :: WType erasure -> Type) where
+  -- | Identifier type.
+  type
+    Ident (erasure :: Erasure) (expr :: WType erasure -> Type)
+      :: WType erasure -> Type
+
+  -- | @int@ literals.
+  intLit :: Ann AtomExpr expr (Int -> expr WInt)
+
+  -- | @bool@ literals.
+  boolLit :: Ann AtomExpr expr (Bool -> expr WBool)
+
+  -- | @char@ literals.
+  charLit :: Ann AtomExpr expr (Char -> expr WChar)
+
+  -- | @string@ literals.
+  stringLit :: Ann AtomExpr expr (String -> expr WString)
+
+  -- | > null
+  null :: Ann AtomExpr expr (expr (WKnownPair t1 t2))
+
+  -- | > <ident>
+  ident :: Ann AtomExpr expr (Ident erasure expr t -> expr t)
+
+  -- | > <ident>[<expr>]...
+  arrayElem
+    :: Ann AtomExpr expr (ArrayElem expr (Ident erasure expr) t -> expr t)
+
+  -- | > (<expr>)
+  parens :: Ann AtomExpr expr (expr t -> expr t)
 
 {- |
 Unary WACC expressions.
