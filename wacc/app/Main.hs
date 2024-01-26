74111b1ae94a93e9c2334a2bb2b5ee400031fdc6
HEIGE-PCloud <heige.pcloud@outlook.com>
2024-01-26 12:01:23 +0000

    feat(main): add skeleton for running the parser

--- wacc/app/Main.hs
+++ wacc/app/Main.hs
@@ -1,7 +1,37 @@
 module Main (main) where
 
-import Lib
+-- import Lib
 
+import GHC.IO.Handle.FD (stderr)
+import GHC.IO.Handle.Text (hPutStrLn)
+import System.Environment (getArgs)
+import System.Exit (ExitCode (ExitFailure), exitSuccess, exitWith, exitFailure)
+import Text.Gigaparsec
+import Text.Gigaparsec.Char
+
+syntaxErrorCode :: Int
+syntaxErrorCode = 100
+semanticErrorCode :: Int
+semanticErrorCode = 200
 
 main :: IO ()
-main = someFunc
+main = do
+  args <- getArgs
+  case args of
+    [filename] -> compile filename
+    _          -> usageAndExit
+
+
+compile :: String -> IO ()
+compile filename = do
+  source <- readFile filename
+  case parse parser source of
+    Failure err -> putStrLn err >> exitWith (ExitFailure syntaxErrorCode)
+    Success res -> print res >> exitSuccess
+
+
+usageAndExit :: IO ()
+usageAndExit = hPutStrLn stderr "Usage: compile <filename>" >> exitFailure
+
+parser :: Parsec Char
+parser = item
