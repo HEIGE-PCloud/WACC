99aea111966d6eba28638d83935fee1a574d41f5
HEIGE-PCloud <heige.pcloud@outlook.com>
2024-01-26 21:45:13 +0000

    feat(lexer): initialise lexer

--- wacc/app/Main.hs
+++ wacc/app/Main.hs
@@ -5,33 +5,50 @@ module Main (main) where
 import GHC.IO.Handle.FD (stderr)
 import GHC.IO.Handle.Text (hPutStrLn)
 import System.Environment (getArgs)
-import System.Exit (ExitCode (ExitFailure), exitSuccess, exitWith, exitFailure)
+import System.Exit (ExitCode (ExitFailure), exitFailure, exitSuccess, exitWith)
 import Text.Gigaparsec
 import Text.Gigaparsec.Char
+import Text.Gigaparsec.Token.Descriptions (LexicalDesc (LexicalDesc, nameDesc, numericDesc, spaceDesc, symbolDesc, textDesc), plainName, plainNumeric, plainSpace, plainSymbol, plainText)
+import Text.Gigaparsec.Token.Lexer (Lexer, fully, mkLexer)
 
 syntaxErrorCode :: Int
 syntaxErrorCode = 100
+
+exitWithSyntaxError :: IO a
+exitWithSyntaxError = exitWith (ExitFailure syntaxErrorCode)
+
 semanticErrorCode :: Int
 semanticErrorCode = 200
+
+exitWithSemanticError :: IO a
+exitWithSemanticError = exitWith (ExitFailure semanticErrorCode)
 
 main :: IO ()
 main = do
   args <- getArgs
   case args of
     [filename] -> compile filename
-    _          -> usageAndExit
-
+    _ -> usageAndExit
 
 compile :: String -> IO ()
 compile filename = do
   source <- readFile filename
   case parse parser source of
-    Failure err -> putStrLn err >> exitWith (ExitFailure syntaxErrorCode)
+    Failure err -> putStrLn err >> exitWithSyntaxError
     Success res -> print res >> exitSuccess
-
 
 usageAndExit :: IO ()
 usageAndExit = hPutStrLn stderr "Usage: compile <filename>" >> exitFailure
 
-parser :: Parsec Char
-parser = item
+lexer :: Lexer
+lexer =
+  mkLexer $
+    LexicalDesc
+      { nameDesc = plainName,
+        symbolDesc = plainSymbol,
+        numericDesc = plainNumeric,
+        textDesc = plainText,
+        spaceDesc = plainSpace
+      }
+
+parser = fully lexer item
