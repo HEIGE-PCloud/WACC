0263431562e3170326a3e105f82c3cc189147fe3
Daniel Trowbridge <daniel.trowbridge22@imperial.ac.uk>
2024-01-28 16:13:20 +0000

    refactor(ast): export all AST declarations from Language.WACC.AST

---
+++ wacc/src/Language/WACC/AST.hs
@@ -0,0 +1,18 @@
+{- |
+Data types, type classes and type families exported by the @Language.WACC.AST.*@
+modules.
+-}
+module Language.WACC.AST
+  ( module Language.WACC.AST.Annotation
+  , module Language.WACC.AST.Expr
+  , module Language.WACC.AST.Prog
+  , module Language.WACC.AST.Stmt
+  , module Language.WACC.AST.WType
+  )
+where
+
+import Language.WACC.AST.Annotation
+import Language.WACC.AST.Expr
+import Language.WACC.AST.Prog
+import Language.WACC.AST.Stmt
+import Language.WACC.AST.WType
