6e1cbdd72911e9b3055ef07a4d693dea9f40fe0f
Daniel Trowbridge <daniel.trowbridge22@imperial.ac.uk>
2024-01-29 13:37:48 +0000

    fix(ast): export Language.WACC.AST.Ident from Language.WACC.AST

--- wacc/src/Language/WACC/AST.hs
+++ wacc/src/Language/WACC/AST.hs
@@ -5,6 +5,7 @@ modules.
 module Language.WACC.AST
   ( module Language.WACC.AST.Annotation
   , module Language.WACC.AST.Expr
+  , module Language.WACC.AST.Ident
   , module Language.WACC.AST.Prog
   , module Language.WACC.AST.Stmt
   , module Language.WACC.AST.WType
@@ -13,6 +14,7 @@ where
 
 import Language.WACC.AST.Annotation
 import Language.WACC.AST.Expr
+import Language.WACC.AST.Ident
 import Language.WACC.AST.Prog
 import Language.WACC.AST.Stmt
 import Language.WACC.AST.WType
