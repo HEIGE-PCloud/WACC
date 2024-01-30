2b80fca28571e01db6697e1c843ad59213b3fa80
HEIGE-PCloud <heige.pcloud@outlook.com>
2024-01-30 22:58:50 +0000

    chore(parser): format style

--- wacc/app/Main.hs
+++ wacc/app/Main.hs
@@ -2,18 +2,14 @@ module Main (main) where
 
 -- import Lib
 
-import qualified Data.Set as Set
 import GHC.IO.Handle.FD (stderr)
 import GHC.IO.Handle.Text (hPutStrLn)
-
+import Language.WACC.Parser.Token
 import System.Environment (getArgs)
 import System.Exit (ExitCode (ExitFailure), exitFailure, exitSuccess, exitWith)
 import Text.Gigaparsec
-import Text.Gigaparsec.Char
-import Text.Gigaparsec.Expr (Fixity (InfixL, InfixN, InfixR, Prefix), Prec (Atom), ops, precedence, (>+))
 import Text.Gigaparsec.Combinator
-import Language.WACC.Parser.Token
-
+import Text.Gigaparsec.Expr (Fixity (InfixL, InfixN, InfixR, Prefix), Prec (Atom), ops, precedence, (>+))
 
 syntaxErrorCode :: Int
 syntaxErrorCode = 100
@@ -112,7 +108,7 @@ pairElemType = baseOrArrayType <|> sym "pair"
 
 program :: Parsec String
 -- program = sym "begin" *> liftA2 (++) (concat <$> many func) stmt <* sym "end"
-program = sym "begin" *>  stmt <* sym "end"
+program = sym "begin" *> stmt <* sym "end"
 
 showMaybe :: Maybe String -> String
 showMaybe Nothing = ""
@@ -128,20 +124,23 @@ param :: Parsec String
 param = liftA2 (++) typeParser ident
 
 stmt :: Parsec String
-stmt = liftA2 (++) (
-      atomic (sym "skip")
-  <|> atomic (param <* sym "=" *> rvalue)
-  <|> lvalue <* sym "=" <* rvalue
-  <|> sym "read" *> lvalue
-  <|> sym "free" *> expr
-  <|> sym "return" *> expr
-  <|> sym "exit" *> expr
-  <|> sym "print" *> expr
-  <|> sym "println" *> expr
-  <|> sym "if" *> expr <* sym "then" *> stmt <* sym "else" *> stmt <* sym "fi"
-  <|> sym "while" *> expr <* sym "do" *> stmt <* sym "done"
-  <|> sym "begin" *> stmt <* sym "end"
-  ) stmtTail
+stmt =
+  liftA2
+    (++)
+    ( atomic (sym "skip")
+        <|> atomic (param <* sym "=" *> rvalue)
+        <|> (lvalue <* sym "=" <* rvalue)
+        <|> (sym "read" *> lvalue)
+        <|> (sym "free" *> expr)
+        <|> (sym "return" *> expr)
+        <|> (sym "exit" *> expr)
+        <|> (sym "print" *> expr)
+        <|> (sym "println" *> expr)
+        <|> (sym "if" *> expr <* sym "then" *> stmt <* sym "else" *> stmt <* sym "fi")
+        <|> (sym "while" *> expr <* sym "do" *> stmt <* sym "done")
+        <|> (sym "begin" *> stmt <* sym "end")
+    )
+    stmtTail
 
 stmtTail :: Parsec String
 stmtTail = showMaybe <$> option (sym ";" *> stmt)
@@ -150,11 +149,17 @@ lvalue :: Parsec String
 lvalue = ident <|> arrayElem <|> pairElem
 
 rvalue :: Parsec String
-rvalue = expr 
-  <|> arrayLiter 
-  <|> (sym "newpair" *> sym "(" *> expr <* sym "," *> expr <* sym ")")
-  <|> pairElem
-  <|> sym "call" *> ident <* sym "(" *> (showMaybe <$> option argList) <* sym ")"
+rvalue =
+  expr
+    <|> arrayLiter
+    <|> (sym "newpair" *> sym "(" *> expr <* sym "," *> expr <* sym ")")
+    <|> pairElem
+    <|> ( sym "call"
+            *> ident
+            <* sym "("
+            *> (showMaybe <$> option argList)
+            <* sym ")"
+        )
 
 argList :: Parsec String
 argList = concat <$> (expr <:> many (sym "," *> expr))
@@ -164,4 +169,3 @@ pairElem = sym "fst" *> lvalue <|> sym "snd" *> lvalue
 
 arrayLiter :: Parsec String
 arrayLiter = sym "[" *> (showMaybe <$> option argList) <* sym "]"
-
