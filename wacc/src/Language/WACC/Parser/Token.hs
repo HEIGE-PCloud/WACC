dbfaa17bc8a88fb1686963da58c5110e80437996
HEIGE-PCloud <heige.pcloud@outlook.com>
2024-01-30 20:00:51 +0000

    refactor(lexer): re-export usable combinators without boilplate

--- wacc/src/Language/WACC/Parser/Token.hs
+++ wacc/src/Language/WACC/Parser/Token.hs
@@ -1,7 +1,7 @@
-{-# LANGUAGE ApplicativeDo #-}
-
 module Language.WACC.Parser.Token
-  ( lexTok
+  ( lexer,
+    lexeme,
+    nonlexeme,
   )
 where
 
@@ -9,188 +9,208 @@ import Data.Char (isAlpha, isAlphaNum, isAscii, isPrint, isSpace)
 import qualified Data.Map as Map
 import qualified Data.Set as Set
 import Text.Gigaparsec.Token.Descriptions
-  ( BreakCharDesc (NoBreakChar)
-  , EscapeDesc
-    ( EscapeDesc
-    , binaryEscape
-    , decimalEscape
-    , emptyEscape
-    , escBegin
-    , gapsSupported
-    , hexadecimalEscape
-    , literals
-    , mapping
-    , octalEscape
-    )
-  , ExponentDesc (NoExponents)
-  , LexicalDesc
-    ( LexicalDesc
-    , nameDesc
-    , numericDesc
-    , spaceDesc
-    , symbolDesc
-    , textDesc
-    )
-  , NameDesc
-    ( NameDesc
-    , identifierLetter
-    , identifierStart
-    , operatorLetter
-    , operatorStart
-    )
-  , NumericDesc
-    ( NumericDesc
-    , binaryExponentDesc
-    , binaryLeads
-    , decimalExponentDesc
-    , hexadecimalExponentDesc
-    , hexadecimalLeads
-    , integerNumbersCanBeBinary
-    , integerNumbersCanBeHexadecimal
-    , integerNumbersCanBeOctal
-    , leadingDotAllowed
-    , leadingZerosAllowed
-    , literalBreakChar
-    , octalExponentDesc
-    , octalLeads
-    , positiveSign
-    , realNumbersCanBeBinary
-    , realNumbersCanBeHexadecimal
-    , realNumbersCanBeOctal
-    , trailingDotAllowed
-    )
-  , NumericEscape (NumericIllegal)
-  , PlusSignPresence (PlusOptional)
-  , SpaceDesc
-    ( SpaceDesc
-    , lineCommentAllowsEOF
-    , lineCommentStart
-    , multiLineCommentEnd
-    , multiLineCommentStart
-    , multiLineNestedComments
-    , space
-    , whitespaceIsContextDependent
-    )
-  , SymbolDesc (SymbolDesc, caseSensitive, hardKeywords, hardOperators)
-  , TextDesc
-    ( TextDesc
-    , characterLiteralEnd
-    , escapeSequences
-    , graphicCharacter
-    , multiStringEnds
-    , stringEnds
-    )
+  ( BreakCharDesc (NoBreakChar),
+    EscapeDesc
+      ( EscapeDesc,
+        binaryEscape,
+        decimalEscape,
+        emptyEscape,
+        escBegin,
+        gapsSupported,
+        hexadecimalEscape,
+        literals,
+        mapping,
+        octalEscape
+      ),
+    ExponentDesc (NoExponents),
+    LexicalDesc
+      ( LexicalDesc,
+        nameDesc,
+        numericDesc,
+        spaceDesc,
+        symbolDesc,
+        textDesc
+      ),
+    NameDesc
+      ( NameDesc,
+        identifierLetter,
+        identifierStart,
+        operatorLetter,
+        operatorStart
+      ),
+    NumericDesc
+      ( NumericDesc,
+        binaryExponentDesc,
+        binaryLeads,
+        decimalExponentDesc,
+        hexadecimalExponentDesc,
+        hexadecimalLeads,
+        integerNumbersCanBeBinary,
+        integerNumbersCanBeHexadecimal,
+        integerNumbersCanBeOctal,
+        leadingDotAllowed,
+        leadingZerosAllowed,
+        literalBreakChar,
+        octalExponentDesc,
+        octalLeads,
+        positiveSign,
+        realNumbersCanBeBinary,
+        realNumbersCanBeHexadecimal,
+        realNumbersCanBeOctal,
+        trailingDotAllowed
+      ),
+    NumericEscape (NumericIllegal),
+    PlusSignPresence (PlusOptional),
+    SpaceDesc
+      ( SpaceDesc,
+        lineCommentAllowsEOF,
+        lineCommentStart,
+        multiLineCommentEnd,
+        multiLineCommentStart,
+        multiLineNestedComments,
+        space,
+        whitespaceIsContextDependent
+      ),
+    SymbolDesc (SymbolDesc, caseSensitive, hardKeywords, hardOperators),
+    TextDesc
+      ( TextDesc,
+        characterLiteralEnd,
+        escapeSequences,
+        graphicCharacter,
+        multiStringEnds,
+        stringEnds
+      ),
   )
-import Text.Gigaparsec.Token.Lexer (Lexer, mkLexer)
+import Text.Gigaparsec.Token.Lexer (Lexeme, Lexer, mkLexer)
+import qualified Text.Gigaparsec.Token.Lexer (Lexer (lexeme, nonlexeme))
 
--- * Lexical Tokens of WACC not directly in structure
+waccNameDesc :: NameDesc
+waccNameDesc =
+  NameDesc
+    { identifierStart = Just (\x -> isAlpha x || x == '_'),
+      identifierLetter = Just (\x -> isAlphaNum x || x == '_'),
+      operatorStart = Nothing,
+      operatorLetter = Nothing
+    }
 
-lexTok :: Lexer
-lexTok =
+waccSymbolDesc :: SymbolDesc
+waccSymbolDesc =
+  SymbolDesc
+    { hardKeywords =
+        Set.fromList
+          [ "begin",
+            "end",
+            "is",
+            "skip",
+            "read",
+            "free",
+            "return",
+            "exit",
+            "println",
+            "print",
+            "if",
+            "else",
+            "fi",
+            "while",
+            "done",
+            "do",
+            "newpair",
+            "call",
+            "fst",
+            "snd"
+          ],
+      hardOperators =
+        Set.fromList
+          [ "!",
+            "-",
+            "len",
+            "ord",
+            "chr",
+            "*",
+            "/",
+            "%",
+            "+",
+            ">",
+            ">=",
+            "<",
+            "<=",
+            "==",
+            "!=",
+            "&&",
+            "||"
+          ],
+      caseSensitive = True
+    }
+
+waccNumericDesc :: NumericDesc
+waccNumericDesc =
+  NumericDesc
+    { literalBreakChar = NoBreakChar,
+      leadingDotAllowed = False,
+      trailingDotAllowed = False,
+      leadingZerosAllowed = True,
+      positiveSign = PlusOptional,
+      integerNumbersCanBeHexadecimal = False,
+      integerNumbersCanBeBinary = False,
+      integerNumbersCanBeOctal = False,
+      realNumbersCanBeBinary = False,
+      realNumbersCanBeHexadecimal = False,
+      realNumbersCanBeOctal = False,
+      hexadecimalLeads = Set.empty,
+      octalLeads = Set.empty,
+      binaryLeads = Set.empty,
+      decimalExponentDesc = NoExponents,
+      hexadecimalExponentDesc = NoExponents,
+      octalExponentDesc = NoExponents,
+      binaryExponentDesc = NoExponents
+    }
+
+waccTextDesc :: TextDesc
+waccTextDesc =
+  TextDesc
+    { escapeSequences =
+        EscapeDesc
+          { escBegin = '\\',
+            literals = Set.fromList ['0', 'b', 't', 'n', 'f', 'r', '"', '\'', '\\'],
+            mapping = Map.empty,
+            decimalEscape = NumericIllegal,
+            octalEscape = NumericIllegal,
+            hexadecimalEscape = NumericIllegal,
+            binaryEscape = NumericIllegal,
+            emptyEscape = Nothing,
+            gapsSupported = False
+          },
+      characterLiteralEnd = '\'',
+      stringEnds = Set.fromList [("\"", "\"")],
+      multiStringEnds = Set.empty,
+      graphicCharacter = Just (\x -> isAscii x && isPrint x)
+    }
+
+waccSpaceDesc :: SpaceDesc
+waccSpaceDesc =
+  SpaceDesc
+    { lineCommentStart = "#",
+      lineCommentAllowsEOF = True,
+      multiLineCommentStart = "",
+      multiLineCommentEnd = "",
+      multiLineNestedComments = False,
+      space = Just isSpace,
+      whitespaceIsContextDependent = False
+    }
+
+lexer :: Lexer
+lexer =
   mkLexer $
     LexicalDesc
-      { nameDesc =
-          NameDesc
-            { identifierStart = Just (\x -> isAlpha x || x == '_')
-            , identifierLetter = Just (\x -> isAlphaNum x || x == '_')
-            , operatorStart = Nothing
-            , operatorLetter = Nothing
-            }
-      , symbolDesc =
-          SymbolDesc
-            { hardKeywords =
-                Set.fromList
-                  [ "begin"
-                  , "end"
-                  , "is"
-                  , "skip"
-                  , "read"
-                  , "free"
-                  , "return"
-                  , "exit"
-                  , "println"
-                  , "print"
-                  , "if"
-                  , "else"
-                  , "fi"
-                  , "while"
-                  , "done"
-                  , "do"
-                  , "newpair"
-                  , "call"
-                  , "fst"
-                  , "snd"
-                  ]
-            , hardOperators =
-                Set.fromList
-                  [ "!"
-                  , "-"
-                  , "len"
-                  , "ord"
-                  , "chr"
-                  , "*"
-                  , "/"
-                  , "%"
-                  , "+"
-                  , ">"
-                  , ">="
-                  , "<"
-                  , "<="
-                  , "=="
-                  , "!="
-                  , "&&"
-                  , "||"
-                  ]
-            , caseSensitive = True
-            }
-      , numericDesc =
-          NumericDesc
-            { literalBreakChar = NoBreakChar
-            , leadingDotAllowed = False
-            , trailingDotAllowed = False
-            , leadingZerosAllowed = True
-            , positiveSign = PlusOptional
-            , integerNumbersCanBeHexadecimal = False
-            , integerNumbersCanBeBinary = False
-            , integerNumbersCanBeOctal = False
-            , realNumbersCanBeBinary = False
-            , realNumbersCanBeHexadecimal = False
-            , realNumbersCanBeOctal = False
-            , hexadecimalLeads = Set.empty
-            , octalLeads = Set.empty
-            , binaryLeads = Set.empty
-            , decimalExponentDesc = NoExponents
-            , hexadecimalExponentDesc = NoExponents
-            , octalExponentDesc = NoExponents
-            , binaryExponentDesc = NoExponents
-            }
-      , textDesc =
-          TextDesc
-            { escapeSequences =
-                EscapeDesc
-                  { escBegin = '\\'
-                  , literals = Set.fromList ['0', 'b', 't', 'n', 'f', 'r', '"', '\'', '\\']
-                  , mapping = Map.empty
-                  , decimalEscape = NumericIllegal
-                  , octalEscape = NumericIllegal
-                  , hexadecimalEscape = NumericIllegal
-                  , binaryEscape = NumericIllegal
-                  , emptyEscape = Nothing
-                  , gapsSupported = False
-                  }
-            , characterLiteralEnd = '\''
-            , stringEnds = Set.fromList [("\"", "\"")]
-            , multiStringEnds = Set.empty
-            , graphicCharacter = Just (\x -> isAscii x && isPrint x)
-            }
-      , spaceDesc =
-          SpaceDesc
-            { lineCommentStart = "#"
-            , lineCommentAllowsEOF = True
-            , multiLineCommentStart = ""
-            , multiLineCommentEnd = ""
-            , multiLineNestedComments = False
-            , Text.Gigaparsec.Token.Descriptions.space = Just isSpace
-            , whitespaceIsContextDependent = False
-            }
+      { nameDesc = waccNameDesc,
+        symbolDesc = waccSymbolDesc,
+        numericDesc = waccNumericDesc,
+        textDesc = waccTextDesc,
+        spaceDesc = waccSpaceDesc
       }
+
+lexeme :: Lexeme
+lexeme = Text.Gigaparsec.Token.Lexer.lexeme lexer
+
+nonlexeme :: Lexeme
+nonlexeme = Text.Gigaparsec.Token.Lexer.nonlexeme lexer
