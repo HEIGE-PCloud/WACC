c02b02f6b82a0dbce56b0d974c86572f49b621b4
HEIGE-PCloud <heige.pcloud@outlook.com>
2024-01-31 01:40:03 +0000

    feat(integration-test): setup valid array test

--- wacc/test/Main.hs
+++ wacc/test/Main.hs
@@ -7,16 +7,73 @@ import System.Exit
 import Test.Program
 import Test.Tasty
 
+basePath :: FilePath
+basePath = "wacc/test/wacc_examples"
+
 main :: IO ()
 main =
   defaultMain $
-    testGroup "Compilation with GHC" $
-      [ testProgram "Foo" "make" [] (Just "..") (ExitSuccess)
+    testGroup
+      "Integration Tests"
+      [ validTests
+        -- syntaxErrorTests,
+        -- semanticErrorTests
       ]
 
 validTests :: TestTree
 validTests =
   testGroup
-    "Valid tests"
-    [ testProgram "Foo" "make" [] Nothing (ExitFailure 2)
+    "Valid"
+    [ validArrayTests
     ]
+
+validArrayTests :: TestTree
+validArrayTests = generateTestGroup "valid" "array" validArrays ExitSuccess
+
+syntaxErrorTests :: TestTree
+syntaxErrorTests =
+  testGroup
+    "Syntax Error"
+    [ testProgram "Foo2" "make" [] Nothing (ExitFailure 100)
+    ]
+
+semanticErrorTests :: TestTree
+semanticErrorTests =
+  testGroup
+    "Semantic Error"
+    [ testProgram "Foo3" "make" [] Nothing (ExitFailure 200)
+    ]
+
+validArrays :: [String]
+validArrays =
+  [ "arrayLength",
+    "lenArrayIndex",
+    "arrayIndexMayBeArrayIndex",
+    "emptyArrayReplace",
+    "stringFromArray",
+    "arrayEmpty",
+    "array",
+    "arrayLookup",
+    "emptyArrayPrint",
+    "free",
+    "arrayOnHeap",
+    "modifyString",
+    "printRef",
+    "emptyArrayNextLine",
+    "arraySimple",
+    "charArrayInStringArray",
+    "arrayNested",
+    "arrayPrint",
+    "emptyArrayScope",
+    "arrayBasic",
+    "emptyArrayAloneIsFine"
+  ]
+
+generateTestCase :: String -> String -> ExitCode -> TestTree
+generateTestCase name path = testProgram name "compile" [path] Nothing
+
+generateTestGroup :: String -> String -> [String] -> ExitCode -> TestTree
+generateTestGroup t st names exitCode =
+  testGroup
+    (t ++ "." ++ st)
+    (map (\name -> generateTestCase name (basePath ++ "/" ++ t ++ "/" ++ st ++ "/" ++ name ++ ".wacc") exitCode) names)
