f33db52dd85b79a94bd70245438c3ee505a23761
Manav Sankriti <ms3822@ic.ac.uk>
2024-01-31 16:50:40 +0000

    feat(Initial-Parser): Implement the basic parser for expressions.

---
+++ wacc/src/Language/WACC/Parser/Expr.hs
@@ -0,0 +1,137 @@
+{-# LANGUAGE ApplicativeDo #-}
+{-# LANGUAGE TemplateHaskell #-}
+
+module Language.WACC.Parser.Expr
+  ( expr
+  )
+where
+
+import Data.Foldable (Foldable (toList))
+import Language.WACC.AST.Expr (ArrayIndex (ArrayIndex), Expr (..), WAtom (..))
+import Language.WACC.Parser.Token
+  ( charLiteral
+  , decimal
+  , identifier
+  , stringLiteral
+  , sym
+  )
+import Text.Gigaparsec (Parsec, (<|>))
+import Text.Gigaparsec.Combinator (choice)
+import Text.Gigaparsec.Combinator.NonEmpty (some)
+import Text.Gigaparsec.Expr
+  ( Fixity (InfixL, InfixN, InfixR, Prefix)
+  , Prec (..)
+  , ops
+  , precedence
+  , (>+)
+  )
+import Text.Gigaparsec.Patterns
+  ( deriveDeferredConstructors
+  , deriveLiftedConstructors
+  )
+import Prelude hiding (GT, LT)
+
+$( deriveLiftedConstructors
+    "mk"
+    ['IntLit, 'BoolLit, 'CharLit, 'StringLit, 'Null, 'Ident, 'ArrayElem]
+ )
+
+$( deriveDeferredConstructors
+    "mk"
+    ['Not, 'Negate, 'Len, 'Ord, 'Chr]
+ )
+
+$( deriveDeferredConstructors
+    "mk"
+    ['Mul, 'Div, 'Mod]
+ )
+
+$( deriveDeferredConstructors
+    "mk"
+    ['Add, 'Sub]
+ )
+
+$( deriveDeferredConstructors
+    "mk"
+    ['GT, 'GTE, 'LT, 'LTE]
+ )
+
+$( deriveDeferredConstructors
+    "mk"
+    ['Eq, 'Ineq]
+ )
+
+$( deriveDeferredConstructors
+    "mk"
+    ['And]
+ )
+
+$( deriveDeferredConstructors
+    "mk"
+    ['Or]
+ )
+
+$( deriveLiftedConstructors
+    "mk"
+    ['WAtom]
+ )
+
+intLiter :: Parsec (WAtom i)
+intLiter = mkIntLit decimal
+
+boolLiter :: Parsec (WAtom i)
+boolLiter = mkBoolLit $ (== "true") <$> (sym "true" <|> sym "false")
+
+charLiter :: Parsec (WAtom i)
+charLiter = mkCharLit charLiteral
+
+stringLiter :: Parsec (WAtom i)
+stringLiter = mkStringLit stringLiteral
+
+pairLiter :: Parsec (WAtom i)
+pairLiter = do
+  sym "null"
+  mkNull
+
+ident :: Parsec (WAtom String)
+ident = mkIdent identifier
+
+arrayElem :: Parsec (WAtom String)
+arrayElem = mkArrayElem $ do
+  s <- identifier
+  exprs <- some (sym "[" *> expr <* sym "]")
+  pure (ArrayIndex s (toList exprs))
+
+atom :: Parsec (WAtom String)
+atom =
+  choice
+    [ intLiter
+    , pairLiter
+    , boolLiter
+    , charLiter
+    , stringLiter
+    , pairLiter
+    , arrayElem
+    , ident
+    ]
+
+expr :: Parsec (Expr String)
+expr =
+  precedence $
+    Atom (mkWAtom atom)
+      >+ ops
+        Prefix
+        [ mkNot <* sym "!"
+        , mkNegate <* sym "-"
+        , mkLen <* sym "len"
+        , mkOrd <* sym "ord"
+        , mkChr <* sym "chr"
+        ]
+      >+ ops InfixL [mkMul <* sym "*", mkMod <* sym "%", mkDiv <* sym "/"]
+      >+ ops InfixL [mkAdd <* sym "+", mkSub <* sym "-"]
+      >+ ops
+        InfixN
+        [mkGT <* sym ">", mkGTE <* sym ">=", mkLT <* sym "<", mkLTE <* sym "<="]
+      >+ ops InfixN [mkEq <* sym "==", mkIneq <* sym "!="]
+      >+ ops InfixR [sym "&&" *> mkAnd]
+      >+ ops InfixR [sym "||" *> mkOr]
