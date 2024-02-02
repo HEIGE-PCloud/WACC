e3e774d1ca29dc40094cf346f7dcccaaf4af5a51
Manav Sankriti <ms3822@ic.ac.uk>
2024-02-02 21:19:12 +0000

    refactor(Parser): Remove string parser defined in Main.hs

--- wacc/app/Main.hs
+++ wacc/app/Main.hs
@@ -16,6 +16,7 @@ import Text.Gigaparsec.Expr
   , precedence
   , (>+)
   )
+import Language.WACC.Parser.Prog (prog)
 
 syntaxErrorCode :: Int
 syntaxErrorCode = 100
@@ -39,159 +40,9 @@ main = do
 compile :: String -> IO ()
 compile filename = do
   source <- readFile filename
-  case parse parser source of
+  case parse prog source of
     Failure err -> putStrLn err >> exitWithSyntaxError
     Success _ -> exitSuccess
 
 usageAndExit :: IO ()
 usageAndExit = hPutStrLn stderr "Usage: compile <filename>" >> exitFailure
-
-parser :: Parsec String
-parser = fully program
-
-intLiter :: Parsec String
-intLiter = show <$> decimal
-
-boolLiter :: Parsec String
-boolLiter = sym "true" <|> sym "false"
-
-charLiter :: Parsec String
-charLiter = show <$> charLiteral
-
-stringLiter :: Parsec String
-stringLiter = stringLiteral
-
-pairLiter :: Parsec String
-pairLiter = sym "null"
-
-ident :: Parsec String
-ident = identifier
-
-arrayElem :: Parsec String
-arrayElem = concat <$> (ident <:> some (sym "[" *> expr <* sym "]"))
-
-atom :: Parsec String
-atom =
-  intLiter
-    <|> pairLiter
-    <|> boolLiter
-    <|> charLiter
-    <|> stringLiteral
-    <|> pairLiter
-    <|> identOrArrayElem
-
-showBinOp :: String -> String -> String -> String
-showBinOp op x y = "(" ++ x ++ op ++ y ++ ")"
-
-binSig :: String -> Parsec (String -> String -> String)
-binSig op = sym op $> showBinOp op
-
-unarySig :: String -> Parsec (String -> String)
-unarySig op = sym op $> \x -> "(" ++ op ++ x ++ ")"
-
-expr :: Parsec String
-expr =
-  precedence $
-    Atom atom
-      >+ ops
-        Prefix
-        [unarySig "!", unarySig "-", unarySig "len", unarySig "ord", unarySig "chr"]
-      >+ ops InfixL [binSig "*", binSig "%", binSig "/"]
-      >+ ops InfixL [binSig "+", binSig "-"]
-      >+ ops InfixN [binSig "<", binSig "<=", binSig ">=", binSig ">"]
-      >+ ops InfixN [binSig "==", binSig "!="]
-      >+ ops InfixR [binSig "&&"]
-      >+ ops InfixR [binSig "||"]
-
-typeParser :: Parsec String
-typeParser = baseOrArrayType <|> pairType
-
-baseType :: Parsec String
-baseType = sym "int" <|> sym "bool" <|> sym "char" <|> sym "string"
-
--- arrayType :: Parsec String
--- arrayType = typeParser <* sym "[]"
-
-baseOrArrayType :: Parsec String
-baseOrArrayType = liftA2 (++) baseType (showMaybe <$> option (sym "[]"))
-
-pairType :: Parsec String
-pairType = sym "pair" *> sym "(" *> pairElemType <* sym "," *> pairElemType <* sym ")"
-
-pairElemType :: Parsec String
-pairElemType = baseOrArrayType <|> sym "pair"
-
-program :: Parsec String
-program = sym "begin" *> many (atomic func) *> stmt <* sym "end"
-
--- program = sym "begin" *> stmt <* sym "end"
-
-showMaybe :: Maybe String -> String
-showMaybe Nothing = ""
-showMaybe (Just x) = x
-
-func :: Parsec String
-func =
-  param
-    <* sym "("
-    *> (showMaybe <$> option paramList)
-    <* sym ")"
-    <* sym "is"
-    *> stmt
-    <* sym "end"
-
-paramList :: Parsec String
-paramList = concat <$> (param <:> many (sym "," *> param))
-
-param :: Parsec String
-param = liftA2 (++) typeParser ident
-
-stmt :: Parsec String
-stmt =
-  liftA2
-    (++)
-    ( atomic (sym "skip")
-        <|> atomic (param <* sym "=" *> rvalue)
-        <|> (lvalue <* sym "=" <* rvalue)
-        <|> (sym "read" *> lvalue)
-        <|> (sym "free" *> expr)
-        <|> (sym "return" *> expr)
-        <|> (sym "exit" *> expr)
-        <|> (sym "print" *> expr)
-        <|> (sym "println" *> expr)
-        <|> (sym "if" *> expr <* sym "then" *> stmt <* sym "else" *> stmt <* sym "fi")
-        <|> (sym "while" *> expr <* sym "do" *> stmt <* sym "done")
-        <|> (sym "begin" *> stmt <* sym "end")
-    )
-    stmtTail
-
-stmtTail :: Parsec String
-stmtTail = showMaybe <$> option (sym ";" *> stmt)
-
-lvalue :: Parsec String
-lvalue = identOrArrayElem <|> pairElem
-
-identOrArrayElem :: Parsec String
-identOrArrayElem = ident *> (many (sym "[" *> expr <* sym "]")) $> ""
-
-rvalue :: Parsec String
-rvalue =
-  expr
-    <|> arrayLiter
-    <|> (sym "newpair" *> sym "(" *> expr <* sym "," *> expr <* sym ")")
-    <|> pairElem
-    <|> ( sym "call"
-            *> ident
-            <* sym "("
-            *> (showMaybe <$> option argList)
-            <* sym ")"
-        )
-
-argList :: Parsec String
-argList = concat <$> (expr <:> many (sym "," *> expr))
-
-pairElem :: Parsec String
-pairElem = sym "fst" *> lvalue <|> sym "snd" *> lvalue
-
-arrayLiter :: Parsec String
-arrayLiter = sym "[" *> (showMaybe <$> option argList) <* sym "]"
