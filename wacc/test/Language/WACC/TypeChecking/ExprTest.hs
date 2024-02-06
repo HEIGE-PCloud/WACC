a006eaf5a127a43a8c4a5399f0b592ce126a3681
Daniel Trowbridge <daniel.trowbridge22@imperial.ac.uk>
2024-02-06 14:11:11 +0000

    test(types): add unit tests for checkExpr

--- wacc/test/Language/WACC/TypeChecking/ExprTest.hs
+++ wacc/test/Language/WACC/TypeChecking/ExprTest.hs
@@ -13,6 +13,7 @@ import Language.WACC.AST.Expr
 import Language.WACC.TypeChecking.BType
 import Language.WACC.TypeChecking.Expr
 import Test
+import Prelude hiding (GT, LT)
 
 btypes :: [BType]
 btypes =
@@ -45,6 +46,88 @@ checkExpr' x = evalStateT (checkExpr x) vars
 
 intExpr :: Expr BType
 intExpr = WAtom $ IntLit 0 undefined
+
+boolExpr :: Expr BType
+boolExpr = WAtom $ BoolLit False undefined
+
+charExpr :: Expr BType
+charExpr = WAtom $ CharLit 'C'
+
+stringExpr :: Expr BType
+stringExpr = WAtom $ StringLit "String"
+
+varExpr :: BType -> Expr BType
+varExpr = WAtom . Ident
+
+showType :: BType -> String
+showType BAny = "T"
+showType BInt = "int"
+showType BBool = "bool"
+showType BChar = "char"
+showType BString = "string"
+showType BErasedPair = "pair"
+showType (BKnownPair t1 t2) =
+  concat ["pair(", showType t1, ", ", showType t2, ")"]
+showType (BArray t) = showType t ++ "[]"
+
+mkBad :: BType -> BType
+mkBad BInt = BBool
+mkBad _ = BInt
+
+testUnOp :: String -> (Expr BType -> Expr BType) -> BType -> BType -> TestTree
+testUnOp name op t ret =
+  testGroup
+    name
+    [ testCase
+        (concat ["accepts ", showType t, "s and returns ", showType ret, "s"])
+        $ checkExpr' (op $ varExpr t) @?= Just ret
+    , testCase ("rejects " ++ showType bad ++ "s") $
+        checkExpr' (op $ varExpr bad) @?= Nothing
+    ]
+  where
+    bad = mkBad t
+
+testBinOp
+  :: String
+  -> (Expr BType -> Expr BType -> Expr BType)
+  -> [(BType, BType, BType)]
+  -> TestTree
+testBinOp name op ts = testGroup name $ ts >>= mkCases
+  where
+    mkCases (t1, t2, ret) =
+      [ testCase
+          ( concat
+              [ accepts
+              , " and returns "
+              , showType ret
+              , "s"
+              ]
+          )
+          $ checkExpr' (op (varExpr t1) (varExpr t2)) @?= Just ret
+      , testCase
+          ( concat
+              [ "rejects "
+              , showType bad1
+              , "s (left only)"
+              ]
+          )
+          $ checkExpr' (op (varExpr bad1) (varExpr t2)) @?= Nothing
+      , testCase
+          ( concat
+              [ "rejects "
+              , showType bad2
+              , "s (right only)"
+              ]
+          )
+          $ checkExpr' (op (varExpr t1) (varExpr bad2)) @?= Nothing
+      ]
+      where
+        accepts
+          | t1 == t2 = "accepts " ++ showType t1 ++ "s"
+          | otherwise =
+              "accepts " ++ showType t1 ++ "s and " ++ showType t2 ++ "s"
+        bad1 = mkBad t1
+        bad2 = mkBad t2
 
 test :: TestTree
 test =
@@ -90,6 +173,61 @@ test =
                 checkAtom'
                   (ArrayElem (ArrayIndex BString [intExpr]))
                   @?= Nothing
+            ]
+        ]
+    , testGroup
+        "checkExpr"
+        [ testGroup
+            "atoms"
+            [ testCase "int atoms are ints" $ checkExpr' intExpr @?= Just BInt
+            , testCase "bool atoms are bools" $
+                checkExpr' boolExpr @?= Just BBool
+            , testCase "char atoms are chars" $
+                checkExpr' charExpr @?= Just BChar
+            , testCase "string atoms are strings" $
+                checkExpr' stringExpr @?= Just BString
+            ]
+        , testGroup
+            "unary operators"
+            [ testUnOp "not" Not BBool BBool
+            , testUnOp "-_ (negate)" Negate BInt BInt
+            , testUnOp "len" Len (BArray BAny) BInt
+            , testUnOp "ord" Ord BChar BInt
+            , testUnOp "chr" Chr BInt BChar
+            ]
+        , testGroup
+            "binary operators"
+            [ testBinOp "_*_ (mul)" Mul [(BInt, BInt, BInt)]
+            , testBinOp "_/_ (div)" Div [(BInt, BInt, BInt)]
+            , testBinOp "_%_ (mod)" Mod [(BInt, BInt, BInt)]
+            , testBinOp "_+_ (add)" Add [(BInt, BInt, BInt)]
+            , testBinOp "_-_ (sub)" Sub [(BInt, BInt, BInt)]
+            , testBinOp
+                "_>_ (gt)"
+                GT
+                [(BInt, BInt, BBool), (BChar, BChar, BBool)]
+            , testBinOp
+                "_>=_ (gte)"
+                GTE
+                [(BInt, BInt, BBool), (BChar, BChar, BBool)]
+            , testBinOp
+                "_<_ (lt)"
+                LT
+                [(t, t, BBool) | t <- orderedTypes]
+            , testBinOp
+                "_<=_ (lte)"
+                LTE
+                [(t, t, BBool) | t <- orderedTypes]
+            , testBinOp
+                "_==_ (eq)"
+                Eq
+                [(t, t, BBool) | t <- btypes, t /= BAny]
+            , testBinOp
+                "_!=_ (ineq)"
+                Eq
+                [(t, t, BBool) | t <- btypes, t /= BAny]
+            , testBinOp "_&&_ (and)" And [(BBool, BBool, BBool)]
+            , testBinOp "_||_ (or)" Or [(BBool, BBool, BBool)]
             ]
         ]
     ]
