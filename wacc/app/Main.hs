650af67be6639364196ea7992886fb56f2187e65
HEIGE-PCloud <heige.pcloud@outlook.com>
2024-02-08 18:12:34 +0000

    feat(integration): integrate everything

--- wacc/app/Main.hs
+++ wacc/app/Main.hs
@@ -8,6 +8,16 @@ import Language.WACC.Parser.Token (fully)
 import System.Environment (getArgs)
 import System.Exit (ExitCode (ExitFailure), exitFailure, exitSuccess, exitWith)
 import Text.Gigaparsec
+import Language.WACC.Semantic.Scope (scopeAnalysis)
+import Language.WACC.AST.Prog (Prog)
+import Language.WACC.AST.WType (WType (..))
+import Language.WACC.Semantic.Scope (Fnident, Vident, VarST)
+import Language.WACC.TypeChecking.Prog (checkProg)
+import Language.WACC.TypeChecking.State (runTypingM)
+import Data.DList (empty)
+import Data.Map ((!))
+import Data.Functor.Foldable
+import Language.WACC.TypeChecking.BType (BType (..), fix)
 
 syntaxErrorCode :: Int
 syntaxErrorCode = 100
@@ -15,27 +25,39 @@ syntaxErrorCode = 100
 exitWithSyntaxError :: IO a
 exitWithSyntaxError = exitWith (ExitFailure syntaxErrorCode)
 
--- semanticErrorCode :: Int
--- semanticErrorCode = 200
+semanticErrorCode :: Int
+semanticErrorCode = 200
 
--- exitWithSemanticError :: IO a
--- exitWithSemanticError = exitWith (ExitFailure semanticErrorCode)
+exitWithSemanticError :: IO a
+exitWithSemanticError = exitWith (ExitFailure semanticErrorCode)
 
 main :: IO ()
 main = do
   args <- getArgs
   case args of
-    [filename] -> compile filename
+    [filename] -> runParse filename
     _ -> usageAndExit
 
-compile :: String -> IO ()
-compile filename = do
+runParse :: String -> IO ()
+runParse filename = do
   sourceCode <- readFile filename
   let
     res = parseWithError (fully program) sourceCode
   case res of
     Failure err -> putStrLn (printError filename (lines sourceCode) err) >> exitWithSyntaxError
-    Success _ -> exitSuccess
+    Success ast -> runScopeAnalysis ast
+
+runScopeAnalysis :: Prog String String -> IO ()
+runScopeAnalysis ast = do
+  case scopeAnalysis ast of
+    Left err -> exitWithSemanticError
+    Right res -> runTypeCheck res
+
+runTypeCheck :: (Prog Fnident Vident, VarST) -> IO ()
+runTypeCheck (ast, sb) = do 
+  let (_, _, err) = runTypingM (checkProg ast) (\ident -> ((fix (fst (sb ! ident))))) undefined 
+  if null err then exitSuccess else exitWithSemanticError
+
 
 usageAndExit :: IO ()
 usageAndExit = hPutStrLn stderr "Usage: compile <filename>" >> exitFailure
