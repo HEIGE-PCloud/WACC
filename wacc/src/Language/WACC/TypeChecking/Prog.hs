c16484e3fc108005d3a5358fcc8d6717df9b3931
Daniel Trowbridge <daniel.trowbridge22@imperial.ac.uk>
2024-02-08 10:11:33 +0000

    feat(types): implement function and program type checking

---
+++ wacc/src/Language/WACC/TypeChecking/Prog.hs
@@ -0,0 +1,33 @@
+{- |
+Type checking actions for WACC programs.
+-}
+module Language.WACC.TypeChecking.Prog where
+
+import Control.Monad (when)
+import Language.WACC.AST.Prog
+import Language.WACC.TypeChecking.BType
+import Language.WACC.TypeChecking.State
+import Language.WACC.TypeChecking.Stmt
+
+{- |
+Type check a WACC function.
+
+Records the type of the function in the 'TypingM' state on success.
+-}
+checkFunc :: (Ord fnident) => Func fnident ident -> TypingM fnident ident ()
+checkFunc (Func rwt f pwts ss) = do
+  let
+    rt = fix rwt
+  setFnType f (FnType (fix . fst <$> pwts) rt)
+  _ <- unifyStmts rt ss
+  pure ()
+
+{- |
+Type check a WACC program.
+-}
+checkProg :: (Ord fnident) => Prog fnident ident -> TypingM fnident ident ()
+checkProg (Main fs ss) = do
+  mapM_ checkFunc fs
+  t <- unifyStmts BAny ss
+  when (t /= BAny) abort
+  pure ()
