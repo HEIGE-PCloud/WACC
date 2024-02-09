9491311d54567ea375c6769003751d8f5cc1da6f
Edwin Fernando <ef322@ic.ac.uk>
2024-02-09 15:39:42 +0000

    chore(lint): fourmolu for source files

--- wacc/src/Language/WACC/Error.hs
+++ wacc/src/Language/WACC/Error.hs
@@ -4,7 +4,16 @@
 {-# LANGUAGE Trustworthy #-}
 {-# LANGUAGE TypeFamilies #-}
 
-module Language.WACC.Error (printError, Error (..), quote, isSuccess, isFailure, syntaxError, semanticError) where
+module Language.WACC.Error
+  ( printError
+  , Error (..)
+  , quote
+  , isSuccess
+  , isFailure
+  , syntaxError
+  , semanticError
+  )
+where
 
 import Data.List.Extra ((!?))
 import Data.List.NonEmpty (NonEmpty)
