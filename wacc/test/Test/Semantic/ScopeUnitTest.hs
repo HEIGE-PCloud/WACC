7687d4e39237cc48ac8ffc0dc97bc5159e26c55f
HEIGE-PCloud <heige.pcloud@outlook.com>
2024-02-09 15:54:40 +0000

    refactor(test): clean up scope check

--- wacc/test/Test/Semantic/ScopeUnitTest.hs
+++ wacc/test/Test/Semantic/ScopeUnitTest.hs
@@ -1,5 +1,8 @@
 {- AUTOCOLLECT.TEST -}
 {-# LANGUAGE TypeApplications #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Use camelCase" #-}
+{-# OPTIONS_GHC -Wno-incomplete-uni-patterns #-}
 
 module Test.Semantic.ScopeUnitTest
   (
@@ -7,12 +10,6 @@ module Test.Semantic.ScopeUnitTest
   )
 where
 
-import Control.Exception
-import Control.Monad.RWS.Strict
-import Control.Monad.Trans.Except
-import qualified Data.DList as DL
-import Data.Either
-import qualified Data.Map as Map
 import Language.WACC.Error
 import Language.WACC.Parser.Stmt
 import Language.WACC.Parser.Token
@@ -20,6 +17,7 @@ import Language.WACC.Semantic.Scope
 import Test.Tasty
 import qualified Test.Tasty.HUnit as T
 import Text.Gigaparsec
+import Language.WACC.AST.Prog (Prog)
 
 test =
   testGroup "unitTests" [testGroup "scopeTests" [naming_fail_tests]]
@@ -29,14 +27,13 @@ hasErrored code = isFailure (scopeAnalysis t)
   where
     (Success t) = parse @String (fully program) code
 
-showErrored code = (scopeAnalysis t)
-  where
-    (Success t) = parse @String (fully program) code
-
+badPairElemFst :: String
 badPairElemFst = "begin\nint x = 5;\nbegin\nfst y = null\nend\nend\n"
 
+badPairElemSnd :: String
 badPairElemSnd = "begin\nint x = 5;\nbegin\nsnd y = null\nend\nend\n"
 
+pair_naming_fail_tests :: TestTree
 pair_naming_fail_tests =
   testGroup
     "Naming Fail Tests - Pair Elem"
@@ -46,16 +43,22 @@ pair_naming_fail_tests =
         T.assertBool "Bad Pair elem snd" (hasErrored badPairElemSnd)
     ]
 
+badRvalueExpr :: String
 badRvalueExpr = "begin\nint x = 5;\nbegin\nx = 5 + y\nend\nend\n"
 
+badRvalueArrayLit :: String
 badRvalueArrayLit = "begin\nint x = 5;\nbegin\nx = [1, 2, y]\nend\nend\n"
 
+badRValueNewPair :: String
 badRValueNewPair = "begin\nint x = 5;\nbegin\nx = newpair(1, y)\nend\nend\n"
 
+badRValuePairElem :: String
 badRValuePairElem = "begin\nint x = 5;\nbegin\nx = fst y\nend\nend\n"
 
+badRValueCall :: String
 badRValueCall = "begin\nint x = 5;\nbegin\nx = call f(y)\nend\nend\n"
 
+rValue_naming_fail_tests :: TestTree
 rValue_naming_fail_tests =
   testGroup
     "Naming Fail Tests - RValue"
@@ -71,12 +74,16 @@ rValue_naming_fail_tests =
         T.assertBool "Bad RValue Call" (hasErrored badRValueCall)
     ]
 
+badLValueIdent :: String
 badLValueIdent = "begin\nint x = 5;\nbegin\ny = 5\nend\nend\n"
 
+badLValueArrayElem :: String
 badLValueArrayElem = "begin\nint x = 5;\nbegin\ny[5] = 5\nend\nend\n"
 
+badLValuePairElem :: String
 badLValuePairElem = "begin\nint x = 5;\nbegin\nfst y = 5\nend\nend\n"
 
+lValue_naming_fail_tests :: TestTree
 lValue_naming_fail_tests =
   testGroup
     "Naming Fail Tests - LValue"
@@ -88,8 +95,10 @@ lValue_naming_fail_tests =
         T.assertBool "Bad LValue PairElem" (hasErrored badLValuePairElem)
     ]
 
+badRenameArrayIndex :: String
 badRenameArrayIndex = "begin\nint[] x = [1, 2];\nbegin\nx[y + 1] = 5\nend\nend\n"
 
+renameArrayIndex_fail_tests :: TestTree
 renameArrayIndex_fail_tests =
   testGroup
     "Naming Fail Tests - Array Index"
@@ -97,18 +106,25 @@ renameArrayIndex_fail_tests =
         T.assertBool "Bad Rename Array Index" (hasErrored badRenameArrayIndex)
     ]
 
+badIdentAtom :: String
 badIdentAtom = "begin\nx = 5\nend\n"
 
+badArrayElemAtom :: String
 badArrayElemAtom = "begin\nx[5] = 5\nend\n"
 
+intLitAtom :: String
 intLitAtom = "begin\nint x = 5\nend\n"
 
+boolLitAtom :: String
 boolLitAtom = "begin\nbool x = true\nend\n"
 
+charLitAtom :: String
 charLitAtom = "begin\nchar x = 'a'\nend\n"
 
+stringLitAtom :: String
 stringLitAtom = "begin\nstring x = \"hello\"\nend\n"
 
+naming_fail_atom_tests :: TestTree
 naming_fail_atom_tests =
   testGroup
     "Naming Fail Tests - Atom"
@@ -126,44 +142,64 @@ naming_fail_atom_tests =
         T.assertBool "String Lit Atom" (not $ hasErrored stringLitAtom)
     ]
 
+badAtomExpr :: String
 badAtomExpr = "begin\nint x = 5;\nbegin\nx = y\nend\nend\n"
 
+badNotExpr :: String
 badNotExpr = "begin\nbool x = false;\nbegin\nx = !y\nend\nend\n"
 
+badNegateExpr :: String
 badNegateExpr = "begin\nint x = 5;\nbegin\nx = -y\nend\nend\n"
 
+badLenExpr :: String
 badLenExpr = "begin\nint x = 2;\nbegin\nx = len y\nend\nend\n"
 
+badOrdExpr :: String
 badOrdExpr = "begin\nint x = 2;\nbegin\nx = ord y\nend\nend\n"
 
+badChrExpr :: String
 badChrExpr = "begin\nchar x = 'a';\nbegin\nx = chr y\nend\nend\n"
 
+badMulExpr :: String
 badMulExpr = "begin\nint x = 2;\nbegin\nx = y * z\nend\nend\n"
 
+badDivExpr :: String
 badDivExpr = "begin\nint x = 2;\nbegin\nx = y / z\nend\nend\n"
 
+badModExpr :: String
 badModExpr = "begin\nint x = 2;\nbegin\nx = y % z\nend\nend\n"
 
+badAddExpr :: String
 badAddExpr = "begin\nint x = 2;\nbegin\nx = y + z\nend\nend\n"
 
+badSubExpr :: String
 badSubExpr = "begin\nint x = 2;\nbegin\nx = y - z\nend\nend\n"
+badGTExpr :: String
 
 badGTExpr = "begin\nbool x = false;\nbegin\nx = y > z\nend\nend\n"
 
+badGTEExpr :: String
 badGTEExpr = "begin\nbool x = false;\nbegin\nx = y >= z\nend\nend\n"
 
+badLTExpr :: String
 badLTExpr = "begin\nbool x = false;\nbegin\nx = y < z\nend\nend\n"
 
+badLTEExpr :: String
 badLTEExpr = "begin\nbool x = false;\nbegin\nx = y <= z\nend\nend\n"
 
+badEqExpr :: String
 badEqExpr = "begin\nbool x = false;\nbegin\nx = y == z\nend\nend\n"
 
+badIneqExpr :: String
 badIneqExpr = "begin\nbool x = false;\nbegin\nx = y != z\nend\nend\n"
 
+badAndExpr :: String
 badAndExpr = "begin\nbool x = false;\nbegin\nx = y && z\nend\nend\n"
 
+badOrExpr :: String
 badOrExpr = "begin\nbool x = false;\nbegin\nx = y || z\nend\nend\n"
 
+naming_fail_expr_tests :: TestTree
 naming_fail_expr_tests =
   testGroup
     "Naming Fail Tests - Expr"
@@ -192,34 +228,49 @@ naming_fail_expr_tests =
     , T.testCase "Bad Or Expr" $ T.assertBool "Bad Or Expr" (hasErrored badOrExpr)
     ]
 
+skipStatement :: String
 skipStatement = "begin\nskip\nend\n"
 
+declareStatement :: String
 declareStatement = "begin\nint x = 5\nend\n"
 
+badAsgnStatement :: String
 badAsgnStatement = "begin\nint x = 5;\nbegin\ny = x\nend\nend\n"
 
+badReadStatement :: String
 badReadStatement = "begin\nint x = 5;\nbegin\nread y\nend\nend\n"
 
+badFreeStatement :: String
 badFreeStatement = "begin\nint x = 5;\nbegin\nfree y\nend\nend\n"
 
+badReturnStatement :: String
 badReturnStatement = "begin\nint x = 5;\nbegin\nreturn y\nend\nend\n"
 
+badExitStatement :: String
 badExitStatement = "begin\nint x = 5;\nbegin\nexit y\nend\nend\n"
 
+badPrintStatement :: String
 badPrintStatement = "begin\nint x = 5;\nbegin\nprint y\nend\nend\n"
 
+badPrintLnStatement :: String
 badPrintLnStatement = "begin\nint x = 5;\nbegin\nprintln y\nend\nend\n"
 
+badIfElseExprstatement :: String
 badIfElseExprstatement = "begin\nint x = 5;\nbegin\nif y then x = 5 else x = 6 fi\nend\nend\n"
 
+badIfElseStatementOneStatement :: String
 badIfElseStatementOneStatement = "begin\nint x = 5;\nbegin\nif true then y = 5 else skip fi\nend\nend\n"
 
+badIfElseStatementTwoStatement :: String
 badIfElseStatementTwoStatement = "begin\nint x = 5;\nbegin\nif true then x = 5 else y = 6 fi\nend\nend\n"
+badWhileExprStatement :: String
 
 badWhileExprStatement = "begin\nint x = 5;\nbegin\nwhile y do x = 5 done\nend\nend\n"
 
+badWhileStatementOneStatement :: String
 badWhileStatementOneStatement = "begin\nint x = 5;\nbegin\nwhile true do y = 5 done\nend\nend\n"
 
+rename_fail_statement_tests :: TestTree
 rename_fail_statement_tests =
   testGroup
     "Naming Fail Tests - Statement"
@@ -259,8 +310,10 @@ rename_fail_statement_tests =
           (hasErrored badWhileStatementOneStatement)
     ]
 
+badStatements :: String
 badStatements = "begin\nint x = 5;\nbegin\nint y = 5;\nz = 5\nend\nend\n"
 
+rename_fail_statements_tests :: TestTree
 rename_fail_statements_tests =
   testGroup
     "Naming Fail Tests - Statements"
@@ -268,6 +321,7 @@ rename_fail_statements_tests =
         T.assertBool "Bad Statements" (hasErrored badStatements)
     ]
 
+naming_fail_tests :: TestTree
 naming_fail_tests =
   testGroup
     "Naming Fail Tests"
