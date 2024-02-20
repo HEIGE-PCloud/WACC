023d23bddde5f2f6cefa6896f5f20c76d981e49f
Manav Sankriti <ms3822@ic.ac.uk>
2024-02-20 12:57:53 +0000

    chore(TAC): Document the Three Address Code Internal Representation

--- wacc/src/Language/WACC/TAC/TAC.hs
+++ wacc/src/Language/WACC/TAC/TAC.hs
@@ -2,72 +2,119 @@
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE KindSignatures #-}
 
+{- |
+Defines the three-address code (TAC) representation of the WACC language.
+-}
 module Language.WACC.TAC.TAC where
 
 import Data.Map (Map)
 import Language.WACC.AST.WType (WType)
 
+{- |
+ADT representing the three-address code (TAC) instructions.
+-}
 data TAC ident lident
-  = BinInstr (Var ident) (Var ident) BinOp (Var ident)
-  | Eqr (Var ident) (Var ident) (Var ident)
-  | InEqr (Var ident) (Var ident) (Var ident)
-  | Eqv (Var ident) (Var ident) (Var ident)
-  | InEqv (Var ident) (Var ident) (Var ident)
-  | UnInstr (Var ident) UnOp (Var ident)
-  | Store (Var ident) (Offset ident) (Var ident)
-  | LoadCI (Var ident) Int (Var ident)
-  | LoadCS (Var ident) String (Var ident)
-  | LoadM (Var ident) (Var ident) (Offset ident)
-  | Call (Var ident) (Label lident) [Var ident]
-  | Print (Var ident) WType
-  | PrintLn (Var ident) WType
-  | Exit (Var ident)
-  | Read (Var ident) WType
-  | Malloc (Var ident) (Offset ident)
-  | Free (Var ident)
+  = -- | > <var> := <var> <binop> <var>
+    BinInstr (Var ident) (Var ident) BinOp (Var ident)
+  | -- |
+    -- > <var> := <var> == <var>
+    --
+    -- where <var> is a @array@ or @pair@
+    Eqr (Var ident) (Var ident) (Var ident)
+  | -- |
+    -- > <var> := <var> != <var>
+    --
+    -- where <var> is a @array@ or @pair@
+    InEqr (Var ident) (Var ident) (Var ident)
+  | -- |
+    -- > <var> := <var> == <var>
+    --
+    -- where <var> is a @int@, @bool@, @char@, or @string@
+    Eqv (Var ident) (Var ident) (Var ident)
+  | -- |
+    -- > <var> := <var> != <var>
+    --
+    -- where <var> is a @int@, @bool@, @char@, or @string@
+    InEqv (Var ident) (Var ident) (Var ident)
+  | -- | > <var> := <unop> <var>
+    UnInstr (Var ident) UnOp (Var ident)
+  | -- | > <var> := <var>[<Offset>]
+    Store (Var ident) (Offset ident) (Var ident) WType
+  | -- | > <var> := Int
+    LoadCI (Var ident) Int
+  | -- | > <var> := String
+    LoadCS (Var ident) String
+  | -- | > <var> := <var>[<Offset>]
+    LoadM (Var ident) (Var ident) (Offset ident) WType
+  | -- |
+    -- > <var> := call <lident>([<var>])
+    --
+    -- where <lident> is a function identifier
+    Call (Var ident) (Label lident) [Var ident]
+  | -- |
+    -- > print <var>
+    --
+    -- where type annotates which print function to use
+    Print (Var ident) WType
+  | -- |
+    -- > println <var>
+    --
+    -- where type annotates which println function to use
+    PrintLn (Var ident) WType
+  | -- |  > exit <var>
+    Exit (Var ident)
+  | -- |
+    -- > <var> := read
+    --
+    -- where input is read from stdin and annotated with type
+    Read (Var ident) WType
+  | -- | > <var> := malloc <Offset>
+    Malloc (Var ident) (Offset ident)
+  | -- | > free <var>
+    Free (Var ident)
 
+-- | Binary operators in TAC
 data BinOp = Add | Sub | Mul | Div | Mod | And | Or | Lt | Gt | Le | Ge
 
+-- | Unary operators in TAC
 data UnOp = Neg | Not
 
+-- | Variables in TAC, either temporary or named in the source code.
 data Var ident = Temp ident | Var ident
 
+-- | Offsets in TAC, either temporary or named in the source code.
 type Offset = Var
 
+-- | Labels in TAC for jumps and function calls to other basic blocks.
 newtype Label lident = Label lident
 
+-- | Jump instructions in TAC Basic Blocks for control flow.
 data Jump ident lident
-  = Jump (Label lident)
-  | CJump (Var ident) (Label lident) (Label lident)
-  | Ret (Var ident)
+  = -- | > jmp <lident>
+    Jump (Label lident)
+  | -- | > cjmp <var> <lident> <lident>
+    CJump (Var ident) (Label lident) (Label lident)
+  | -- | > ret <var>
+    Ret (Var ident)
 
+-- | Block labels in TAC for basic blocks.
 newtype BlockLabel ident = BlockLabel ident
 
+-- | Function representation in TAC.
 data Func ident lident
-  = Func lident [Var ident] (Map ident (BasicBlock ident lident))
+  = Func
+      lident
+      -- ^ label of the function body
+      [Var ident]
+      -- ^ parameters of the functions
+      (Map lident (BasicBlock ident lident))
+      -- ^ Map of the related sub basic blocks in function body
 
+-- | A basic block consisting a sequence of TAC instructions and a jump to the next block
 data BasicBlock ident lident = BasicBlock
   { block :: [TAC ident lident]
   , nextBlock :: Jump ident lident
   }
 
+-- | The top-level mapping of function identifiers to their corresponding blocks.
 type TACProgram ident lident = Map lident (Func ident lident)
-
-{-
-int f() is
-  int x = 1;
-  int y = 2;
-  return x + y
-end
-
-int g() is
-  int x = 3;
-  int y = 4;
-  return x + y
-end
-
-int main() is
-  int z = f();
-  return z
-end
--}
