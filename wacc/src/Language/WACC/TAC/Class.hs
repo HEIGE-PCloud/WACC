58e4795cd90797c57073bf82720cf41b5a44f9ce
Daniel Trowbridge <daniel.trowbridge22@imperial.ac.uk>
2024-02-21 00:20:31 +0000

    feat(TAC): allocate fresh labels and temporary variables using state monad

--- wacc/src/Language/WACC/TAC/Class.hs
+++ wacc/src/Language/WACC/TAC/Class.hs
@@ -5,6 +5,8 @@ Type class for translating AST nodes into three-address code.
 -}
 module Language.WACC.TAC.Class (ToTAC (..)) where
 
+import Language.WACC.TAC.State (TACM)
+
 {- |
 AST nodes which can be translated into three-address code.
 -}
@@ -12,5 +14,11 @@ class ToTAC a where
   -- | Three-address code representation type (from "Language.WACC.TAC.TAC").
   type TACRepr a
 
+  -- | Identifier type.
+  type TACIdent a
+
+  -- | Label type.
+  type TACLIdent a
+
   -- | Translate an AST node.
-  toTAC :: a -> TACRepr a
+  toTAC :: a -> TACM (TACIdent a) (TACLIdent a) (TACRepr a)
