41e02c6016e7ee0a42853e91f6e78647b761940d
Edwin Fernando <ef322@ic.ac.uk>
2024-02-25 16:48:07 +0000

    chore(x86->tac): comment functions

--- wacc/src/Language/WACC/X86/Translate.hs
+++ wacc/src/Language/WACC/X86/Translate.hs
@@ -34,27 +34,6 @@ import Language.WACC.X86.X86
   )
 import qualified Language.WACC.X86.X86 as X86
 
-{- | A caller saved register. When registers run out and values in the stack are
-required for a computation, they are swapped with the value in this register.
-This is only required when there are both operands are in memory.
--}
-swapReg :: Register
-swapReg = R10
-
-data Allocation = Allocation
-  { getAlloc :: Bimap (Var Integer) Operand
-  , translated :: Set (TAC.Label Integer)
-  , stackVarNum :: Integer
-  , runtimeFns :: Set Runtime
-  , freeRegs :: [Register]
-  }
-
-type Analysis =
-  RWS
-    (Map Integer (BasicBlock Integer Integer))
-    (DList Instr)
-    Allocation
-
 -- | Translate every function's instructions and concat
 translateProg :: TACProgram Integer Integer -> Prog
 translateProg = D.toList . foldr (D.append . translateFunc) D.empty . M.elems
@@ -69,9 +48,38 @@ translateFunc (Func l v bs) = is
       evalRWS
         (translateBlocks (TAC.Label n) startBlock)
         bs
-        (Allocation B.empty S.empty 0 S.empty []) -- Not empty regs list
+        (TransST B.empty S.empty 0 S.empty []) -- Not empty regs list
     (n, startBlock) = M.findMin bs
 
+{- | When registers run out and values in the stack are
+required for a computation, they put in this register.
+A caller saved register.
+-}
+swapReg :: Register
+swapReg = R10
+
+-- | The state of RWS monad for translation of each function
+data TransST = TransST
+  { alloc :: Bimap (Var Integer) Operand
+  -- ^ bijection between variables in TAC to register/memory in X86
+  , translated :: Set (TAC.Label Integer)
+  -- ^ Set of basic blocks that have been translated
+  , stackVarNum :: Integer
+  -- ^ The number of stack variables used so far (not incl. saving callee saved reg)
+  , runtimeFns :: Set Runtime
+  -- ^ Set of runtime functions which need to be included
+  , freeRegs :: [Register]
+  -- ^ Unused registers
+  }
+
+-- | Reader maps labels to basic blocks. Writer holds output X86 program
+type Analysis =
+  RWS
+    (Map Integer (BasicBlock Integer Integer))
+    (DList Instr)
+    TransST
+
+-- | translate each statement of the block. then figure out which block to go to
 translateBlocks
   :: TAC.Label Integer
   -> BasicBlock Integer Integer
@@ -80,14 +88,17 @@ translateBlocks l (BasicBlock is next) = do
   translateBlock l is
   translateNext next
 
+-- | modify the state of the RWS monad
 puts :: (Monoid w) => (s -> s) -> RWS r w s ()
 puts f = do
   s <- get
   put (f s)
 
-setTranslated :: (TAC.Label Integer) -> Allocation -> Allocation
-setTranslated l x@(Allocation {translated}) = x {translated = (S.insert l) translated}
+-- | Mark the block as translated, so its not re-translated
+setTranslated :: (TAC.Label Integer) -> TransST -> TransST
+setTranslated l x@(TransST {translated}) = x {translated = (S.insert l) translated}
 
+-- | translate each TAC statement
 translateBlock
   :: TAC.Label Integer
   -> [TAC Integer Integer]
@@ -106,6 +117,10 @@ tellInstr = tell . D.singleton
 isTranslated :: TAC.Label Integer -> Analysis Bool
 isTranslated l = gets ((S.member l) . translated)
 
+{- | Flow control: For CJump translate l2 first then translate l1 after returning from the recursive call
+This generates weird (but correct) assembly with else first then if. The if block appears roughly at the
+end of the recursive call.
+-}
 translateNext :: Jump Integer Integer -> Analysis ()
 translateNext (Jump l1@(Label n)) = do
   nextBlock <- asks (! n)
@@ -114,7 +129,7 @@ translateNext (Jump l1@(Label n)) = do
     False -> translateBlocks l1 nextBlock
     True -> tellInstr (Jmp (mapLab l1))
 translateNext (CJump v l1 l2@(TAC.Label n2)) = do
-  operand <- gets ((B.! v) . getAlloc)
+  operand <- gets ((B.! v) . alloc)
   tellInstr (Cmpq operand (Imm 0))
   tellInstr (Jne (mapLab l1)) -- jump to l1 if v != 0. Otherwise keep going
   translateNext (Jump l2)
@@ -124,25 +139,18 @@ translateNext (CJump v l1 l2@(TAC.Label n2)) = do
     True -> pure ()
 translateNext (TAC.Ret var) = pure ()
 
--- getVar :: Var Integer -> Analysis Register
--- getVar v = do
---  vLoc <- gets (! v)
---  case vLoc of
---    Imm i -> error "This is not supposed to happen by pre-condition"
---    Reg r -> return (Reg r)
---    Mem m -> do undefined
+addAlloc :: Var Integer -> Operand -> TransST -> TransST
+addAlloc v o x@(TransST {alloc}) = x {alloc = (B.insert v o) alloc}
 
-addAlloc :: Var Integer -> Operand -> Allocation -> Allocation
-addAlloc v o x@(Allocation {getAlloc}) = x {getAlloc = (B.insert v o) getAlloc}
-
+-- | Free a register if none are available by pushing the swapReg onto stack
 getReg :: Analysis Register
 getReg = do
   rs <- gets freeRegs
   case rs of
     [] -> do
       tellInstr (Pushq (Reg swapReg))
-      swapVar <- gets ((B.!> (Reg swapReg)) . getAlloc)
-      puts (\x@(Allocation {stackVarNum}) -> x {stackVarNum = stackVarNum + 1})
+      swapVar <- gets ((B.!> (Reg swapReg)) . alloc)
+      puts (\x@(TransST {stackVarNum}) -> x {stackVarNum = stackVarNum + 1})
       stackAddr <- (gets ((* 4) . stackVarNum))
       puts (addAlloc swapVar (Mem (MRegI stackAddr Rbp)))
       return swapReg
@@ -158,16 +166,12 @@ translateTAC (BinInstr lv rv1 op rv2) = do
   -- always assign new variables to a register
   r <- getReg
   puts (addAlloc lv (Reg r))
-  op1 <- gets ((B.! rv1) . getAlloc)
-  op2 <- gets ((B.! rv2) . getAlloc)
+  op1 <- gets ((B.! rv1) . alloc)
+  op2 <- gets ((B.! rv2) . alloc)
   tellInstr (Movq op1 (Reg r))
   tellInstr ((translateBinOp op) op2 (Reg r))
   tellInstr (Jo (R ErrOverflow))
 
--- translateTAC (EqR (Var ident) (Var ident) (Var ident)
--- translateTAC (IneqR (Var ident) (Var ident) (Var ident)
--- translateTAC (EqV (Var ident) (Var ident) (Var ident)
--- translateTAC (IneqV (Var ident) (Var ident) (Var ident)
 -- translateTAC (UnInstr (Var ident) UnOp (Var ident)
 -- translateTAC (Store (Var ident) (Offset ident) (Var ident) WType
 -- translateTAC (LoadCI (Var ident) Int
@@ -179,5 +183,4 @@ translateTAC (BinInstr lv rv1 op rv2) = do
 -- translateTAC (Exit (Var ident)
 -- translateTAC (Read (Var ident) WType
 -- translateTAC (TAC.Malloc lv rv) = translateTAC (TAC.Call lv (R X86.Malloc) [rv])
-
 -- translateTAC (Free (Var ident)
