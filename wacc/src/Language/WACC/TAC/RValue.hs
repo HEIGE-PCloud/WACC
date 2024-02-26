a1b3abf72d010e0cd7354606e777541b8d3e6aff
Daniel Trowbridge <daniel.trowbridge22@imperial.ac.uk>
2024-02-26 17:24:55 +0000

    feat(TAC): translate RVArrayLit

--- wacc/src/Language/WACC/TAC/RValue.hs
+++ wacc/src/Language/WACC/TAC/RValue.hs
@@ -9,11 +9,13 @@ TAC translation actions for WACC @rvalue@s.
 -}
 module Language.WACC.TAC.RValue where
 
+import Control.Monad (zipWithM)
+import Data.List.NonEmpty (fromList)
 import Data.Semigroup (sconcat)
 import Language.WACC.AST
 import Language.WACC.TAC.Class
 import Language.WACC.TAC.Expr (ExprTACs (..), loadCI)
-import Language.WACC.TAC.FType (flatten)
+import Language.WACC.TAC.FType
 import Language.WACC.TAC.State
 import Language.WACC.TAC.TAC
 import Language.WACC.TypeChecking
@@ -27,6 +29,35 @@ instance
   type TACFnRepr (RValue fnident ident BType) = ExprTACs ident fnident
   type TACFnIdent (RValue fnident ident BType) = fnident
   fnToTAC (RVExpr x _) = toTAC x
+  fnToTAC (RVArrayLit xs (BArray t)) =
+    [ sconcat
+      [ sconcat' calcXsTACs
+      , c0ts
+      , loadArrayLengthTACs
+      , loadArraySizeTACs
+      , [ Malloc array (exprV loadArraySizeTACs)
+        , Store array (exprV c0ts) (exprV loadArrayLengthTACs) FInt
+        ]
+          array
+      , sconcat' storeXsTACs
+      , [] array
+      ]
+    | calcXsTACs <- mapM toTAC xs
+    , c0ts <- loadCI 0
+    , loadArrayLengthTACs <- loadCI elemCount
+    , loadArraySizeTACs <- loadCI (elemCount * elemSize + sizeOf FInt)
+    , array <- freshTemp
+    , storeXsTACs <- zipWithM (storeAtIndex array) calcXsTACs [0 ..]
+    ]
+    where
+      sconcat' = sconcat . fromList
+      ft = flatten t
+      elemCount = length xs
+      elemSize = sizeOf ft
+      storeAtIndex array xts i =
+        [ cts <> [Store array (exprV cts) (exprV xts) ft] array
+        | cts <- loadCI (elemSize * i + sizeOf FInt)
+        ]
   fnToTAC (RVNewPair x y _) =
     [ sconcat
       [ xts
