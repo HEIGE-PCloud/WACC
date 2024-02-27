5f01d55a9714eccf878451e06c42d5793ba7b7ad
Daniel Trowbridge <daniel.trowbridge22@imperial.ac.uk>
2024-02-27 17:41:05 +0000

    fix(TAC): update RValue translations to use RWS transformer stack

--- wacc/src/Language/WACC/TAC/RValue.hs
+++ wacc/src/Language/WACC/TAC/RValue.hs
@@ -1,5 +1,4 @@
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE MonadComprehensions #-}
 {-# LANGUAGE OverloadedLists #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# OPTIONS_GHC -Wno-orphans #-}
@@ -9,12 +8,10 @@ TAC translation actions for WACC @rvalue@s.
 -}
 module Language.WACC.TAC.RValue where
 
-import Control.Monad (zipWithM)
-import Data.List.NonEmpty (fromList)
-import Data.Semigroup (sconcat)
+import Control.Monad (zipWithM_)
 import Language.WACC.AST
 import Language.WACC.TAC.Class
-import Language.WACC.TAC.Expr (ExprTACs (..), loadCI)
+import Language.WACC.TAC.Expr ()
 import Language.WACC.TAC.FType
 import Language.WACC.TAC.State
 import Language.WACC.TAC.TAC
@@ -26,55 +23,36 @@ instance
   (Enum fnident, Enum ident)
   => FnToTAC (RValue fnident ident BType)
   where
-  type TACFnRepr (RValue fnident ident BType) = ExprTACs ident fnident
+  type TACFnRepr (RValue fnident ident BType) = ()
   type TACFnIdent (RValue fnident ident BType) = fnident
   fnToTAC (RVExpr x _) = toTAC x
-  fnToTAC (RVArrayLit xs (BArray t)) =
-    [ sconcat
-      [ sconcat' calcXsTACs
-      , c0ts
-      , loadArrayLengthTACs
-      , loadArraySizeTACs
-      , [ Malloc array (exprV loadArraySizeTACs)
-        , Store array (exprV c0ts) (exprV loadArrayLengthTACs) FInt
-        ]
-          array
-      , sconcat' storeXsTACs
-      , [] array
-      ]
-    | calcXsTACs <- mapM toTAC xs
-    , c0ts <- loadCI 0
-    , loadArrayLengthTACs <- loadCI elemCount
-    , loadArraySizeTACs <- loadCI (elemCount * elemSize + sizeOf FInt)
-    , array <- freshTemp
-    , storeXsTACs <- zipWithM (storeAtIndex array) calcXsTACs [0 ..]
-    ]
-    where
-      sconcat' = sconcat . fromList
+  fnToTAC (RVArrayLit xs (BArray t)) = do
+    target <- getTarget
+    let
       ft = flatten t
       elemCount = length xs
       elemSize = sizeOf ft
-      storeAtIndex array xts i =
-        [ cts <> [Store array (exprV cts) (exprV xts) ft] array
-        | cts <- loadCI (elemSize * i + sizeOf FInt)
-        ]
-  fnToTAC (RVNewPair x y _) =
-    [ sconcat
-      [ xts
-      , yts
-      , c0ts
-      , c8ts
-      , c16ts
-      , [ Malloc t (exprV c16ts)
-        , Store t (exprV c0ts) (exprV xts) (flatten $ getAnn x)
-        , Store t (exprV c8ts) (exprV yts) (flatten $ getAnn y)
-        ]
-          t
+      evaluateAndStore x i = do
+        temp <- tempWith (toTAC x)
+        offset <- loadConst (elemSize * i + sizeOf FInt)
+        putTACs [Store target offset temp ft]
+    arraySize <- loadConst (elemSize * elemCount + sizeOf FInt)
+    arrayLength <- loadConst elemCount
+    lengthOffset <- loadConst 0
+    putTACs
+      [ Malloc target arraySize
+      , Store target lengthOffset arrayLength FInt
       ]
-    | xts <- toTAC x
-    , yts <- toTAC y
-    , c0ts <- loadCI 0
-    , c8ts <- loadCI 8
-    , c16ts <- loadCI 16
-    , t <- freshTemp
-    ]
+    zipWithM_ evaluateAndStore xs [0 ..]
+  fnToTAC (RVNewPair x y _) = do
+    temp1 <- tempWith (toTAC x)
+    temp2 <- tempWith (toTAC y)
+    target <- getTarget
+    fstOffset <- loadConst 0
+    sndOffset <- loadConst 8
+    pairSize <- loadConst 16
+    putTACs
+      [ Malloc target pairSize
+      , Store target fstOffset temp1 (flatten $ getAnn x)
+      , Store target sndOffset temp2 (flatten $ getAnn y)
+      ]
