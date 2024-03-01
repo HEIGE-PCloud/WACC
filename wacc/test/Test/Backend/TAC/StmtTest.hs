186ca084836c15d5ab906ddab3bf3979f04c03ce
Manav Sankriti <ms3822@ic.ac.uk>
2024-03-01 17:49:12 +0000

    feat(TAC_Test): Implement initial tests for Assignment and Declaration

--- wacc/test/Test/Backend/TAC/StmtTest.hs
+++ wacc/test/Test/Backend/TAC/StmtTest.hs
@@ -25,8 +25,8 @@ toTAC' = testTACM . (*> ((,) <$> collectTACs <*> collectBlocks)) . fnToTAC
 intLit :: (Integral a) => a -> Expr Int BType
 intLit x = WAtom (IntLit (toInteger x) BInt) BInt
 
-temp0, temp1, temp2, temp3, temp4, temp5, temp6 :: Var Int
-temp0 : temp1 : temp2 : temp3 : temp4 : temp5 : temp6 : _ = Temp <$> [0 ..]
+temp0, temp1, temp2, temp3, temp4, temp5, temp6, temp7, temp8 :: Var Int
+temp0 : temp1 : temp2 : temp3 : temp4 : temp5 : temp6 : temp7 : temp8 : _ = Temp <$> [0 ..]
 
 stmtTestGroup :: TestTree
 stmtTestGroup =
@@ -45,6 +45,107 @@ stmtTestGroup =
     , testProperty "println uses PrintLn" $ \i ->
         toTAC' (AST.PrintLn (intLit i) BAny)
           === ([LoadCI temp1 i, PrintLn temp1 FInt], [])
+    , testProperty "Decl uses Decl" $ \v i1 i2 ->
+        toTAC'
+          ( AST.Decl
+              WInt
+              v
+              ( AST.RVExpr
+                  ( AST.Add
+                      (intLit i1)
+                      (intLit i2)
+                      BInt
+                  )
+                  BInt
+              )
+              BAny
+          )
+          === (
+                [ LoadCI temp1 i1
+                , LoadCI temp2 i2
+                , BinInstr (Var v) temp1 Language.WACC.TAC.TAC.Add temp2
+                ]
+              , []
+              )
+    , testProperty "Asgn uses Asgn with LVIdent" $ \v i1 i2 ->
+        toTAC'
+          ( AST.Asgn
+              (AST.LVIdent v BInt)
+              ( AST.RVExpr
+                  ( AST.Add
+                      (intLit i1)
+                      (intLit i2)
+                      BInt
+                  )
+                  BInt
+              )
+              BAny
+          )
+          === (
+                [ LoadCI temp1 i1
+                , LoadCI temp2 i2
+                , BinInstr (Var v) temp1 Language.WACC.TAC.TAC.Add temp2
+                ]
+              , []
+              )
+    , testProperty "Asgn uses Asgn with LVArrayElem" $ \v e i1 i2 ->
+        toTAC'
+          ( AST.Asgn
+              ( AST.LVArrayElem
+                  ( AST.ArrayIndex v [intLit e] (BArray BInt)
+                  )
+                  BInt
+              )
+              ( AST.RVExpr
+                  ( AST.Add
+                      (intLit i1)
+                      (intLit i2)
+                      BInt
+                  )
+                  BInt
+              )
+              BAny
+          )
+          === (
+                [ LoadCI temp1 4
+                , LoadCI temp2 4
+                , LoadCI temp3 e
+                , BinInstr temp4 temp3 Language.WACC.TAC.TAC.Mul temp2
+                , BinInstr temp5 temp4 Language.WACC.TAC.TAC.Add temp1
+                , LoadCI temp7 i1
+                , LoadCI temp8 i2
+                , BinInstr temp6 temp7 Language.WACC.TAC.TAC.Add temp8
+                , Store (Var v) temp5 temp6 WIntF
+                ]
+              , []
+              )
+    , testProperty "Asgn uses Asgn with LVPairElem" $ \v i1 i2 ->
+        toTAC'
+          ( AST.Asgn
+              ( AST.LVPairElem
+                  ( AST.FstElem (LVIdent v BErasedPair) BInt
+                  )
+                  BInt
+              )
+              ( AST.RVExpr
+                  ( AST.Add
+                      (intLit i1)
+                      (intLit i2)
+                      BInt
+                  )
+                  BInt
+              )
+              BAny
+          )
+          === (
+                [ LoadCI temp1 v
+                , LoadCI temp3 i1
+                , LoadCI temp4 i2
+                , BinInstr temp2 temp3 Language.WACC.TAC.TAC.Add temp4
+                , Store temp0 temp1 temp2 WIntF
+                ]
+              , []
+              )
     ]
 
 jump0 :: Jump Int Int
