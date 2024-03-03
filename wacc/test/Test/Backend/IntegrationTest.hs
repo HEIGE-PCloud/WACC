68cbffddb8e35a9574bbbf5f2e0afc9e1fb11c3f
HEIGE-PCloud <heige.pcloud@outlook.com>
2024-03-03 12:23:31 +0000

    chore(integration-test): switch on all tests

--- wacc/test/Test/Backend/IntegrationTest.hs
+++ wacc/test/Test/Backend/IntegrationTest.hs
@@ -36,9 +36,9 @@ import Text.Gigaparsec.Token.Lexer
   )
 
 enabledTests :: [FilePath]
-enabledTests = [t | t <- allTests, any (`isInfixOf` t) enabledPaths]
-  where
-    enabledPaths = ["basic", "variables", "sequence", "epxressions", "if"]
+enabledTests = allTests -- [t | t <- allTests, any (`isInfixOf` t) enabledPaths]
+-- where
+--   enabledPaths = ["basic", "variables", "sequence", "epxressions", "if"]
 
 allTests :: [FilePath]
 allTests = [t | t <- validTests, not $ "advanced" `isInfixOf` t]
